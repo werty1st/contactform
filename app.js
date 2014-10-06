80293082ed87a5cce13b5fb7db19d6bd46d01462
werty1st <adams@werty1st.de>
2014-10-06 17:55:36 +0200

    sendmail

---
+++ app.js
@@ -0,0 +1,26 @@
+
+var nodemailer = require('nodemailer');
+
+// create reusable transporter object using SMTP transport
+var transporter = nodemailer.createTransport();
+
+// NB! No need to recreate the transporter object. You can use
+// the same transporter object for all e-mails
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: 'Fred Foo ✔ <kontakt@zdf.de>', // sender address
+    to: 'adams.r@zdf.de', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world ✔', // plaintext body
+    html: '<b>Hello world ✔</b>' // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+    }else{
+        console.log('Message sent: ' + info.response);
+    }
+});
